6cd4157c1880b5c72c0d910fbc00e6da37cc31b7
Stas <uririomai@yandex.ru>
2024-12-18 23:23:31 +0500

    add validation, add close popup overlay, keydown

--- scripts/index.js
+++ scripts/index.js
@@ -76,9 +76,13 @@ initialCards.map(({ name, link }) => renderCard(name, link))
 
 const openModal = popup => {
 	popup.classList.add('popup_is-opened')
+
+	document.addEventListener('keydown', closeByEsc)
 }
 const closeModal = popup => {
 	popup.classList.remove('popup_is-opened')
+
+	document.removeEventListener('keydown', closeByEsc)
 }
 
 profileEditButton.addEventListener('click', () => openModal(profilePopup))
@@ -87,9 +91,18 @@ profileAddButton.addEventListener('click', () => openModal(cardPopup))
 popups.forEach(popup => {
 	const buttonClose = popup.querySelector('.popup__close')
 	buttonClose.addEventListener('click', () => closeModal(popup))
-
+	popup.addEventListener('mousedown', function (e) {
+		if (e.target.classList.contains('popup')) closeModal(popup)
+	})
 	popup.classList.add('popup_is-animated')
 })
+
+function closeByEsc(evt) {
+	if (evt.key === 'Escape') {
+		const openedPopup = document.querySelector('.popup_is-opened')
+		closeModal(openedPopup)
+	}
+}
 
 nameInputProfilePopup.value = profileName.textContent
 descriptInputProfilePopup.value = profileDescript.textContent
@@ -119,3 +132,93 @@ cardList.addEventListener('click', function (evt) {
 		evt.target.classList.toggle('card__like-button_is-active')
 	}
 })
+
+// Валидация
+
+const hasInvalidInput = inputList => {
+	return inputList.some(inputElement => {
+		return !inputElement.validity.valid
+	})
+}
+
+const toggleButtonState = (inputList, buttonElement, validationSettings) => {
+	if (hasInvalidInput(inputList)) {
+		buttonElement.classList.add(validationSettings.inactiveButtonClass)
+		buttonElement.disabled = true
+	} else {
+		buttonElement.classList.remove(validationSettings.inactiveButtonClass)
+		buttonElement.disabled = false
+	}
+}
+
+const showInputError = (
+	formElement,
+	inputElement,
+	errorMessage,
+	validationSettings
+) => {
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+	inputElement.classList.add(validationSettings.inputErrorClass)
+	errorElement.textContent = errorMessage
+	errorElement.classList.add(validationSettings.errorClass)
+}
+
+const hideInputError = (formElement, inputElement, validationSettings) => {
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+	inputElement.classList.remove(validationSettings.inputErrorClass)
+	errorElement.classList.remove(validationSettings.errorClass)
+	errorElement.textContent = ''
+}
+
+const isValid = (formElement, inputElement, validationSettings) => {
+	if (!inputElement.validity.valid) {
+		showInputError(
+			formElement,
+			inputElement,
+			inputElement.validationMessage,
+			validationSettings
+		)
+	} else {
+		hideInputError(formElement, inputElement, validationSettings)
+	}
+}
+
+const setEventListeners = (formElement, validationSettings) => {
+	const inputList = Array.from(
+		formElement.querySelectorAll(validationSettings.inputSelector)
+	)
+	const buttonElement = formElement.querySelector(
+		validationSettings.submitButtonSelector
+	)
+
+	toggleButtonState(inputList, buttonElement, validationSettings)
+
+	inputList.forEach(inputElement => {
+		inputElement.addEventListener('input', () => {
+			isValid(formElement, inputElement, validationSettings)
+
+			toggleButtonState(inputList, buttonElement, validationSettings)
+		})
+	})
+}
+
+const enableValidation = validationSettings => {
+	const formList = Array.from(
+		document.querySelectorAll(validationSettings.formSelector)
+	)
+
+	formList.forEach(formElement => {
+		setEventListeners(formElement, validationSettings)
+	})
+}
+
+const validationSettings = {
+	formSelector: '.popup__form',
+	inputSelector: '.popup__input',
+	submitButtonSelector: '.popup__button',
+	inactiveButtonClass: 'popup__button_disabled',
+	inputErrorClass: 'popup__input_type_error',
+	errorClass: 'popup__error_visible',
+}
+
+enableValidation(validationSettings)
